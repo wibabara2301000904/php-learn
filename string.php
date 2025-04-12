82b6eaa5bb956ee8885fd8a22cfd1c232ef6c287
wibabara2301000904 <fezaelizabeth1@gmail.com>
2025-04-12 19:57:13 +0200

    Add files via upload

---
+++ string.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    <title>My first PHP Website</title>
+    </head>
+    <body>
+        <?php 
+        $x = "Feza ";
+       echo "Hello $x";
+        ?>
+    </body>
+</html>
