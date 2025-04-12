94e622926a6d0798c998b94e05d28190c4cf7d49
wibabara2301000904 <fezaelizabeth1@gmail.com>
2025-04-12 18:54:47 +0200

    Add files via upload
    first assignment

---
+++ site.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    <title>My first PHP Website</title>
+    </head>
+    <body>
+        <?php
+            echo "<p> Feza testing now  Learning php <br>
+            this is the php codes we are running </p>";
+        ?>
+    </body>
+</html>
